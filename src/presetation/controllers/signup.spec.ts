d8a3d606d3fca3e8db0fb5397fc11e44a488076b
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-26 20:28:38 -0300

    refactor: add customized error for missing param

--- src/presetation/controllers/signup.spec.ts
+++ src/presetation/controllers/signup.spec.ts
@@ -1,33 +1,34 @@
-import { SignUpController } from "./signup";
+import { MissingParamError } from '../errors/missing-param-error';
+import { SignUpController } from './signup';
 
-describe("SignUp Controller", () => {
-  test("Should return 400 if no name is provided", () => {
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
       },
     };
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 
-  test("Should return 400 if no email is provided", () => {
+  test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        name: "any_name",
-        password: "any_password",
-        passwordConfirmation: "any_password",
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
       },
     };
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 });
--- src/presetation/controllers/signup.ts
+++ src/presetation/controllers/signup.ts
@@ -1,18 +1,18 @@
+import { MissingParamError } from '../errors/missing-param-error';
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
 
 export class SignUpController {
-  // eslint-disable-next-line consistent-return
   handle(httpRequest: IHttpRequest): IHttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name'),
+        body: new MissingParamError('name'),
       };
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email'),
+        body: new MissingParamError('email'),
       };
     }
   }
