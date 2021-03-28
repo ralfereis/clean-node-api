c48dc247aab7fbd1d47dd53036fdd00ecd5046db
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-27 23:06:54 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**',
+  ],
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -44,6 +44,7 @@ export class SignUpController implements IController {
       });
       return ok(account);
     } catch (error) {
+      console.error(error);
       return serverError();
     }
   }
