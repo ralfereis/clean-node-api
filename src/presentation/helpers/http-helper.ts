e8f2443e923830c975096fdaf960fbe869017189
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-27 22:19:12 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { ServerError } from '../errors/server-error';
+import { ServerError } from '../errors';
 import { IHttpResponse } from '../protocols/http';
 
 export const badRequest = (error: Error): IHttpResponse => ({
