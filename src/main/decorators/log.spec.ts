dcb9d03be8f2eca170b0df2e1bc52c3d0dcf7770
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-02 20:00:03 -0300

    refactor: move sut creation to a factory method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,53 @@
+// eslint-disable-next-line max-classes-per-file
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse,
+} from '../../presentation/protocols';
+import { LogControllerDecorator } from './log';
+
+interface ISutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: IController;
+}
+
+const makeController = (): IController => {
+  class ControllerStub implements IController {
+    async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+      const httpResponse: IHttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Ralfe Reis',
+        },
+      };
+      return new Promise(resolve => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): ISutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
+describe('Log Controller Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
