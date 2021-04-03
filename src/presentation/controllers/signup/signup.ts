9d07944ed3b632b64643c37389d2d2001a1b2f4d
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-02 21:05:03 -0300

    refactor: make server error returns the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -44,7 +44,7 @@ export class SignUpController implements IController {
       });
       return ok(account);
     } catch (error) {
-      return serverError();
+      return serverError(error);
     }
   }
 }
