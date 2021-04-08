7f04a507d93c4b81f21cc54b4f7478621368524a
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-07 21:52:44 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { IAccountModel } from '../usecases/add-account/db-add-account-protocols';
+
+export interface ILoadAccountByEmailRepository {
+  load(email: string): Promise<IAccountModel>;
+}
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import {
+  IAuthentication,
+  IAuthenticationModel,
+} from '../../../domain/usecases/authentication';
+import { ILoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+
+export class DbAuthentication implements IAuthentication {
+  private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository;
+  constructor(loadAccountByEmailRepository: ILoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+  }
+  async auth(authentication: IAuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email);
+    return null;
+  }
+}
