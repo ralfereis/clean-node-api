5809461db9797e62fbdc7f99344898f8d1737f98
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-11 18:19:59 -0300

    test: ensure AccountMongoRepository returns null if LoadEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -35,4 +35,25 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@mail.com');
     expect(account.password).toBe('any_password');
   });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    const account = await sut.loadByEmail('any_email@mail.com');
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut();
+    const account = await sut.loadByEmail('any_email@mail.com');
+    expect(account).toBeFalsy();
+  });
 });
