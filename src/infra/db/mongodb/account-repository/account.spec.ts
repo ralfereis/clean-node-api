8c1774ea513498858cf916be6fa237350bd744c1
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-11 18:42:29 -0300

    test: ensure AccountMongoRepository updates the account accessToken un updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { Collection } from 'mongodb';
 
 import { MongoHelper } from '../helpers/mongo-helper';
@@ -55,5 +56,21 @@ describe('Account Mongo Repository', () => {
     const sut = makeSut();
     const account = await sut.loadByEmail('any_email@mail.com');
     expect(account).toBeFalsy();
+  });
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut();
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    const fakeAccount = result.ops[0];
+    expect(fakeAccount.accessToken).toBeFalsy();
+    await sut.updateAccessToken(fakeAccount._id, 'any_token');
+    const account = await accountCollection.findOne({ _id: fakeAccount._id });
+    expect(account).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.accessToken).toBe('any_token');
   });
 });
