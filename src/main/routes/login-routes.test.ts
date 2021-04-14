bbbe99ab56e51969cc6e8c145f1ba33cdb40ba10
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-13 21:15:54 -0300

    refactor: rename signup routes with login reoutes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -15,15 +15,17 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Ralfe Reis',
-        email: 'ralfe-dev@outlook.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Ralfe Reis',
+          email: 'ralfe-dev@outlook.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
   });
 });
