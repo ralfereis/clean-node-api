d8218daa89be11ce5120933daf8f9f74f3969b96
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-18 22:59:22 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCasewith corrects values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface ISurveyAnswer {
+  image: string;
+  answer: string;
+}
+
+export interface IAddSurveyModel {
+  question: string;
+  answers: ISurveyAnswer[];
+}
+
+export interface IAddSurvey {
+  add(data: IAddSurveyModel): Promise<void>;
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,18 +1,27 @@
 import { badRequest } from '../../../helpers/http/http-helper';
-import { IValidation } from '../../../protocols';
 import {
+  IAddSurvey,
   IController,
   IHttpRequest,
   IHttpResponse,
+  IValidation,
 } from './add-survey-controller-protocols';
 
 export class AddSurveyController implements IController {
-  constructor(private readonly validation: IValidation) {}
+  constructor(
+    private readonly validation: IValidation,
+    private readonly addSurvey: IAddSurvey,
+  ) {}
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     const error = this.validation.validate(httpRequest.body);
     if (error) {
       return badRequest(error);
     }
-    return new Promise(resolve => resolve(null));
+    const { question, answers } = httpRequest.body;
+    await this.addSurvey.add({
+      question,
+      answers,
+    });
+    return null;
   }
 }
