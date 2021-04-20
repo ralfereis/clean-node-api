555b96e36a65c0cef1ca4ecfba50803ca07df33b
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-19 22:57:09 -0300

    refactor: change the path makeDbAddAccount to the right import path folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller';
 import { IController } from '../../../../presentation/protocols';
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory';
-import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory';
+import { makeDbAddAccount } from '../../usecases/add-survey/db-add-survey-factory';
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory';
 import { makeSignUpValidation } from './signup-validation-factory';
 
