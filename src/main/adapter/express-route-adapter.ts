cdb955432d140cace0e70ec79b63f73d3601853b
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-24 19:01:43 -0300

    fix: make controllers returns the errors message instead of the error name

--- src/main/adapter/express-route-adapter.ts
+++ src/main/adapter/express-route-adapter.ts
@@ -7,7 +7,7 @@ export const adaptRoute = (controller: IController) => {
       body: request.body,
     };
     const httpResponse = await controller.handle(httpRequest);
-    if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       response.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
       response
