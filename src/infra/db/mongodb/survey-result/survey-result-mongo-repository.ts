dd7036f04edc7de53b3e7af4d7eec22a3e18718f
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-25 17:22:47 -0300

    feat: ensure SurveyResultMongoRepository insert a new surveyResult its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,32 @@
+import {
+  ISaveSurveyResultRepository,
+  SaveSurveyResultModel,
+  SurveyResultModel,
+} from '@/data/usecases/save-survey-result/db-save-survey-result-protocols';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class SurveyResultMongoRepository
+  implements ISaveSurveyResultRepository {
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection(
+      'surveyResults',
+    );
+    const result = await surveyResultCollection.findOneAndUpdate(
+      {
+        surveyId: data.surveyId,
+        accountId: data.accountId,
+      },
+      {
+        $set: {
+          answer: data.answer,
+          date: data.date,
+        },
+      },
+      {
+        upsert: true,
+        returnOriginal: false,
+      },
+    );
+    return result.value && MongoHelper.map(result.value);
+  }
+}
