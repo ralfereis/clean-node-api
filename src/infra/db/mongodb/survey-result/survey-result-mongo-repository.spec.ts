b684034b06128ac7d07475aae1e8f2c0d4bb9b4f
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-25 18:00:21 -0300

    test: ensure SurveyResultMongoRepository update surveyResult its nota new one

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -21,6 +21,9 @@ const makeSurvey = async (): Promise<SurveyModel> => {
         image: 'any_image',
         answer: 'any_answer',
       },
+      {
+        answer: 'other_answer',
+      },
     ],
     date: new Date(),
   });
@@ -53,7 +56,7 @@ describe('Survey Mongo Repository', () => {
   });
 
   describe('save()', () => {
-    test('Should add a survey result if its new one', async () => {
+    test('Should add a survey result if its a new one', async () => {
       const survey = await makeSurvey();
       const account = await makeAccount();
       const sut = makeSut();
@@ -66,6 +69,27 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult).toBeTruthy();
       expect(surveyResult.id).toBeTruthy();
       expect(surveyResult.answer).toBe(survey.answers[0].answer);
+    });
+
+    test('Should update survey result if its not a new one', async () => {
+      const survey = await makeSurvey();
+      const account = await makeAccount();
+      const result = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date(),
+      });
+      const sut = makeSut();
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date(),
+      });
+      expect(surveyResult).toBeTruthy();
+      expect(surveyResult.id).toEqual(result.ops[0]._id);
+      expect(surveyResult.answer).toBe(survey.answers[1].answer);
     });
   });
 });
