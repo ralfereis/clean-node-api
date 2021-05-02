4a8cf3033b94c3c2d0509b3e85461093aee2d1e4
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-02 00:23:40 -0300

    test: add noCache middleware

---
+++ src/main/middlewares/no-cache.test.ts
@@ -0,0 +1,20 @@
+import app from '../config/app';
+import request from 'supertest';
+import { noCache } from './no-cache';
+
+describe('NoCache Middleware', () => {
+  test('Should disable cache', async () => {
+    app.get('/test_no-cache', noCache, (request, response) => {
+      response.send();
+    });
+    await request(app)
+      .get('/test_no-cache')
+      .expect(
+        'cache-control',
+        'no-store, no-cache, must-revalidate, proxy-revalidate',
+      )
+      .expect('pragma', 'no-cache')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store');
+  });
+});
