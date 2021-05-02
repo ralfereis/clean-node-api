947d6e127e15956d0e93749680a6752ccdc3ed09
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-02 00:00:35 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Typescript API',
+    description:
+      'Api do curso do Mango para realizar enquetes entre programadores ',
+    version: '1.0.0',
+  },
+};
