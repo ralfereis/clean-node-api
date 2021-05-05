291d855b9439054090ad63e817f97c92d3984be8
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-05 19:20:37 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,18 +1,34 @@
+/* eslint-disable max-classes-per-file */
 import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { mockSurveyResultModel } from '@/domain/test';
 import { SurveyResultModel } from '../survey-result/save-survey-result/db-save-survey-result-protocols';
 import { DbLoadSurveyResult } from './db-load-survey-result';
 
+const mockLoadSurveyResultRepository = (): ILoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements ILoadSurveyResultRepository {
+    async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel());
+    }
+  }
+  return new LoadSurveyResultRepositoryStub();
+};
+type SutTypes = {
+  sut: DbLoadSurveyResult;
+  loadSurveyResultRepositoryStub: ILoadSurveyResultRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository();
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+  return {
+    sut,
+    loadSurveyResultRepositoryStub,
+  };
+};
+
 describe('DbLoadSurveyResult UseCase', () => {
   test('should call LoadSurveyResultRepository', async () => {
-    class LoadSurveyResultRepositoryStub
-      implements ILoadSurveyResultRepository {
-      async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
-        return Promise.resolve(mockSurveyResultModel());
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub();
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
     const loadBySurveyIdSpy = jest.spyOn(
       loadSurveyResultRepositoryStub,
       'loadBySurveyId',
