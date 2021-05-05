3acf59aa7fe3c15d83825618a6a988461a8823f2
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-05 19:10:27 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,23 @@
+import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
+import { mockSurveyResultModel } from '@/domain/test';
+import { SurveyResultModel } from '../survey-result/save-survey-result/db-save-survey-result-protocols';
+import { DbLoadSurveyResult } from './db-load-survey-result';
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('should call LoadSurveyResultRepository', async () => {
+    class LoadSurveyResultRepositoryStub
+      implements ILoadSurveyResultRepository {
+      async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+        return Promise.resolve(mockSurveyResultModel());
+      }
+    }
+    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub();
+    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+    const loadBySurveyIdSpy = jest.spyOn(
+      loadSurveyResultRepositoryStub,
+      'loadBySurveyId',
+    );
+    await sut.load('any_survey_id');
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id');
+  });
+});
