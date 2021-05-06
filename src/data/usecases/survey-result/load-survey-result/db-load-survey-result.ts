0f8adde1f81782032db51619e4fc141cd0d073fb
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-06 20:15:53 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -11,11 +11,21 @@ export class DbLoadSurveyResult implements ILoadSurveyResult {
     private readonly loadSurveyByIdRepository: ILoadSurveyByIdRepository,
   ) {}
   async load(surveyId: string): Promise<SurveyResultModel> {
-    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
+    let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
       surveyId,
     );
     if (!surveyResult) {
-      await this.loadSurveyByIdRepository.loadById(surveyId);
+      const survey = await this.loadSurveyByIdRepository.loadById(surveyId);
+      surveyResult = {
+        surveyId: survey.id,
+        question: survey.question,
+        date: survey.date,
+        answers: survey.answers.map(answer => ({
+          ...answer,
+          count: 0,
+          percent: 0,
+        })),
+      };
     }
     return surveyResult;
   }
--- src/domain/test/mock-survey.ts
+++ src/domain/test/mock-survey.ts
@@ -7,8 +7,11 @@ export const mockSurveyModel = (): SurveyModel => {
     question: 'any_question',
     answers: [
       {
-        image: 'any_image',
         answer: 'any_answer',
+      },
+      {
+        answer: 'other_answer',
+        image: 'any_image',
       },
     ],
     date: new Date(),
