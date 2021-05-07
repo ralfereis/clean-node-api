706985c1d8cedc27c6c0b25e545b238049fef69c
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-07 16:12:55 -0300

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -7,6 +7,8 @@ export const surveyResultPath = {
     ],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description:
+      'Essa rota só pode ser executada por **usuários autenticados**',
     parameters: [
       {
         in: 'path',
@@ -26,6 +28,48 @@ export const surveyResultPath = {
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description:
+      'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
