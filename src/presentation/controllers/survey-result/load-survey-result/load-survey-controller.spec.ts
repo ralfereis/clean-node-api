6ea6ab2e5a940af7a5571c4c476d8a45ec3785b8
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-06 23:53:11 -0300

    test: ensure LoadSurveyResultController returns 500 if LoadSurveyResult throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-controller.spec.ts
@@ -70,4 +70,11 @@ describe('LoadSurveyResult Controller', () => {
     await sut.handle(mockRequest());
     expect(loadSpy).toHaveBeenCalledWith('any_id');
   });
+
+  test('Should return 500 if LoadSurveyResult throws', async () => {
+    const { sut, loadSurveyResultStub } = makeSut();
+    jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError);
+    const httpResponse = await sut.handle(mockRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
