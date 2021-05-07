a2a09e0d3cbb2b92f3d4fa396e1b2b90bb022b39
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-07 15:57:36 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -83,5 +83,26 @@ describe('Survey Routes', () => {
     test('Should return 403 on load survey result without accessToken', async () => {
       await request(app).get('/api/surveys/any_id/results').expect(403);
     });
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      const result = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com',
+          },
+          {
+            answer: 'Answer 2',
+          },
+        ],
+        date: new Date(),
+      });
+      await request(app)
+        .get(`/api/surveys/${result.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200);
+    });
   });
 });
