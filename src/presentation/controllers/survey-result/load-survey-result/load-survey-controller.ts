f780afc8cc4fdc2617b7030c9907cced0829668e
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-06 23:59:01 -0300

    feat: ensure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-controller.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError } from '@/presentation/errors';
 import {
   forbidden,
+  ok,
   serverError,
 } from '@/presentation/helpers/http/http-helper';
 import {
@@ -23,8 +24,8 @@ export class LoadSurveyResultController implements IController {
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'));
       }
-      await this.loadSurveyResult.load(surveyId);
-      return null;
+      const surveyResult = await this.loadSurveyResult.load(surveyId);
+      return ok(surveyResult);
     } catch (error) {
       return serverError(error);
     }
