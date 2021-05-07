5f075d77f1be69bce3fd9ce160450f4bec69f7aa
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-07 16:31:08 -0300

    test: ensure SurveyMongoRepository returns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -144,5 +144,12 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult.answers[2].count).toBe(0);
       expect(surveyResult.answers[2].percent).toBe(0);
     });
+    test('Should return null if there is no survey result', async () => {
+      const survey = await makeSurvey();
+      const account = await makeAccount();
+      const sut = makeSut();
+      const surveyResult = await sut.loadBySurveyId(survey.id);
+      expect(surveyResult).toBeNull();
+    });
   });
 });
