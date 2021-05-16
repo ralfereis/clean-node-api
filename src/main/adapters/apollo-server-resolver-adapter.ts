ab28ba2076e69c9ec8aef606fc8b909694f1ad55
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-16 19:01:58 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,9 +1,27 @@
 import { IController } from '@/presentation/protocols';
+import {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError,
+  ApolloError,
+} from 'apollo-server-errors';
 
 export const adaptResolver = async (
   controller: IController,
   args: any,
 ): Promise<any> => {
   const httpResponse = await controller.handle(args);
-  return httpResponse.body;
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204:
+      return httpResponse.body;
+    case 400:
+      throw new UserInputError(httpResponse.body.message);
+    case 401:
+      throw new AuthenticationError(httpResponse.body.message);
+    case 403:
+      throw new ForbiddenError(httpResponse.body.message);
+    default:
+      throw new ApolloError(httpResponse.body.message);
+  }
 };
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -3,11 +3,41 @@ import resolvers from '@/main/graphql/resolvers';
 
 import { ApolloServer } from 'apollo-server-express';
 import { Express } from 'express';
+import { GraphQLError } from 'graphql';
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(
+    name => name === errorName,
+  );
+};
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined;
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400;
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401;
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403;
+    } else {
+      response.http.status = 500;
+    }
+  });
+};
 
 export default (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
     typeDefs,
+    plugins: [
+      {
+        requestDidStart: () => ({
+          willSendResponse: ({ response, errors }) =>
+            handleErrors(response, errors),
+        }),
+      },
+    ],
   });
   server.applyMiddleware({ app });
 };
