dafdd14633988544d5309adf614021f070f49a8f
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-17 20:20:59 -0300

    feat: add auth directive to surveys query

--- src/main/graphql/type-defs/base.ts
+++ src/main/graphql/type-defs/base.ts
@@ -2,9 +2,13 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   scalar DateTime
+
+  directive @auth on FIELD_DEFINITION
+
   type Query {
     _: String
   }
+
   type Mutation {
     _: String
   }
