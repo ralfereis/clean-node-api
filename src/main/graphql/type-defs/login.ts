6a5c370ec7deda326698276c57defa050e48d298
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-05-17 18:19:46 -0300

    feat: add singup mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -4,6 +4,16 @@ export default gql`
   extend type Query {
     login(email: String!, password: String!): Account!
   }
+
+  extend type Mutation {
+    signUp(
+      name: String!
+      email: String!
+      password: String!
+      passwordConfirmation: String!
+    ): Account!
+  }
+
   type Account {
     accessToken: String!
     name: String!
